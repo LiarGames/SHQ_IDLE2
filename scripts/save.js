7c240b45966511eac68f331e5718b28ce1eb00b3
LiarGames <sem2646@naver.com>
2025-11-23 15:50:49 +0900

    merge error fix

--- scripts/save.js
+++ scripts/save.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const sunday=42;
 const friday="SHQ_IDLE2_";
 
@@ -20,27 +19,4 @@ function localLoad(key)
     for(let i=0; i<arr.length; i++)
         arr[i] = String.fromCharCode(Number(arr[i])-sunday);
     return JSON.parse(arr.join(''));
-=======
-const sunday=42;
-const friday="SHQ_IDLE2_";
-
-function localSave(key,data)
-{
-    let str = JSON.stringify(data);
-    let arr=[];
-    for(let i=0; i<str.length; i++)
-        arr[i] = str.charCodeAt(i)+sunday;
-    localStorage.setItem(friday+key,arr.toString());
-}
-
-function localLoad(key)
-{
-    let str = localStorage.getItem(friday+key);
-    if(str===null)
-        return -1;
-    let arr = str.split(',');
-    for(let i=0; i<arr.length; i++)
-        arr[i] = String.fromCharCode(Number(arr[i])-sunday);
-    return JSON.parse(arr.join(''));
->>>>>>> 666df278a63db5de2b1beeb2ad7951a8568572c7
 }
